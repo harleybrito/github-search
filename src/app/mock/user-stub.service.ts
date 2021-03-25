b03ef7ca29c537f1f120400f732eb8b1643a816f
Harley de Brito <harleydebrito@gmail.com>
2021-03-25 18:08:02 -0300

    Testes unitários de UserScreenComponentcompletos
    100% de cobertura dos testes do UserScreenComponent.

--- src/app/mock/user-stub.service.ts
+++ src/app/mock/user-stub.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Observable, Subscriber } from 'rxjs';
 import { User } from '../models/user';
 import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable()
 
@@ -20,8 +20,16 @@ export class UserStubService {
         avatar_url: 'https://avatars.githubusercontent.com/u/22543873?s=460&u=43853b8e79164b80279b1d73ce2d43ce6f971f31&v=4'
     };
 
-    public get(): Observable<User> {
+    public get(username: string): Observable<User> {
         return new Observable<User>((subscriber: Subscriber<User>) => {
+            if(username == '/404'){
+                throw new HttpErrorResponse({ status: 404 });
+            }
+
+            if(username == '/400'){
+                throw new HttpErrorResponse({ status: 400 });
+            }
+
             subscriber.next(this.user);
             subscriber.complete();
         });
---
+++ src/app/screens/user-screen/user-screen.component.spec.ts
@@ -0,0 +1,69 @@
+import { RepoService } from './../../services/repo.service';
+import { RouterStubService } from './../../mock/router-stub.service';
+import { ActivatedRouteStubService } from '../../mock/activated-route-stub.service';
+import { AppModule } from './../../app.module';
+import { TestBed, ComponentFixture, fakeAsync, flush, flushMicrotasks } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UserScreenComponent } from './user-screen.component';
+import { UserService } from 'src/app/services/user.service';
+import { UserStubService } from 'src/app/mock/user-stub.service';
+import { RepoStubService } from 'src/app/mock/repo-stub.service';
+
+
+
+describe('UserScreenComponent', () => {
+    let component: UserScreenComponent;
+    let fixture: ComponentFixture<UserScreenComponent>;
+    let debugElement: DebugElement;
+    let router: Router;
+    let routeSnapshot: ActivatedRoute;
+    let userService: UserService;
+    let repoService: RepoService;
+    
+    beforeEach(async () => {
+        TestBed.configureTestingModule({
+            declarations: [ UserScreenComponent ],
+            imports: [ AppModule ],
+            providers: [
+                { provide: Router, useValue: new RouterStubService() },
+                { provide: ActivatedRoute, useValue: new ActivatedRouteStubService },
+                { provide: UserService, useValue: new UserStubService() },
+                { provide: RepoService, useValue: new RepoStubService() },
+                { provide: Router, useValue: new RouterStubService() }
+            ]
+        }).compileComponents().then(() => {
+            fixture = TestBed.createComponent(UserScreenComponent);
+            component = fixture.componentInstance;
+            debugElement = fixture.debugElement;
+            router = TestBed.inject(Router);
+            routeSnapshot = TestBed.inject(ActivatedRoute);
+            userService = TestBed.inject(UserService);
+            repoService = TestBed.inject(RepoService);
+            spyOn(router, 'navigate').and.callThrough();
+            spyOn(component, 'search').and.callThrough();
+            fixture.detectChanges();
+        });
+    });
+
+    it('should be created', () => {
+        expect(component.fieldValue).toMatch(new ActivatedRouteStubService().params['username']);
+        expect(component).toBeDefined();
+        expect(component.search).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call a not found error', () => {
+        component.search('404');
+    });
+
+    it('should call a connection error', () => {
+        component.search('400');
+    });
+
+    // it('should call "onReturnButtonClicked" and navigate', () => {
+    //     const button: DebugElement = debugElement.query(By.css('button'));
+    //     button.nativeElement.dispatchEvent(new Event('click'));
+    //     expect(component.onReturnButtonClicked).toHaveBeenCalledTimes(1);
+    //     expect(router.navigate).toHaveBeenCalled();
+    // });
+});
